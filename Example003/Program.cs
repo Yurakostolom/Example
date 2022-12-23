ff7fc589c67e297487d31ff6ecd7969ebe0ee1c5
Yura Abramov <yurakostolom@gmail.com>
2022-12-23 22:27:26 +0300

    random

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int numberA= 3;
-int NumberB= 5;
-Console.WriteLine(numberA+ NumberB);
+int numberA= new Random().Next(1,10); // 1 2 3 4 ... 9
+int numberB= new Random().Next(1,10);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+
+Console.WriteLine(numberA+ numberB);
