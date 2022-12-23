da915f4c0145cb1637a76b965a2e12184acb417a
Yura Abramov <yurakostolom@gmail.com>
2022-12-23 22:36:16 +0300

    сделали программу приветствия

---
+++ Example005/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это Маша!");
+
+}
+else
+{
+Console.Write("Привет, ");
+Console.WriteLine(username);
+
+}
